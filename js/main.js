d2584dd3c9e7a34999ffb7fb5729035937f2d1d4
Vignesh Pai <55633408+VigneshPai-hub@users.noreply.github.com>
2021-11-07 01:29:00 +0530

    themes and accents completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,74 @@
 
+/*------------------navigation menu--------------------------*/
+
+(() => {
+    const hamburgerBtn = document.querySelector(".hamburger-btn"),
+        navMenu = document.querySelector(".nav-menu"),
+        closeNavBtn = navMenu.querySelector(".close-nav-menu");
+
+    hamburgerBtn.addEventListener("click", showNavMenu);
+    closeNavBtn.addEventListener("click", hideNavMenu);
+
+    function showNavMenu() {
+        navMenu.classList.add("open");
+        bodyScrollingToggle();
+    }
+
+    function hideNavMenu() {
+        navMenu.classList.remove("open");
+        fadeOutEffect();
+        bodyScrollingToggle();
+    }
+
+    function fadeOutEffect() {
+        document.querySelector(".fade-out-effect").classList.add("active");
+        setTimeout(() => {
+            document.querySelector(".fade-out-effect").classList.remove("active");
+        }, 300);
+    }
+
+    //attach an event handler to the document
+    document.addEventListener("click", (event) => {
+        if (event.target.classList.contains('link-item')) {
+            /* make sure event.target.hash has a value before overriding default behaviour */
+            if (event.target.hash !== "") {
+                /* prevent default anchor click behavior */
+                event.preventDefault();
+                const hash = event.target.hash;
+                /* deactivate existing active section */
+                document.querySelector(".section.active").classList.add("hide");
+                document.querySelector(".section.active").classList.remove("active");
+                /* activate new section */
+                document.querySelector(hash).classList.add("active");
+                document.querySelector(hash).classList.remove("hide");
+                /*deactivate active navigation menu "link-item" */
+                navMenu.querySelector(".active").classList.add("outer-shadow", "hover-in-shadow");
+                navMenu.querySelector(".active").classList.remove("active", "inner-shadow");
+
+                if (navMenu.classList.contains("open")) {
+                    //activate new navigation menu "link-item"
+                    event.target.classList.add("active", "inner-shadow");
+                    event.target.classList.remove("outer-shadow", "hover-in-shadow");
+                    /* hide navigation menu */
+                    hideNavMenu();
+                } else {
+                    let navItems = navMenu.querySelectorAll(".link-item");
+                    navItems.forEach(item => {
+                        if (hash === item.hash) {
+                            item.classList.add("active", "inner-shadow");
+                            item.classList.remove("outer-shadow", "hover-in-shadow");
+                        }
+                    });
+                    fadeOutEffect();
+                }
+                //add hash (#) to the URL
+                window.location.hash = hash;
+            }
+        }
+    });
+
+
+})();
 
 /*-------------------about section tabs--------------------------*/
 
@@ -25,7 +95,7 @@
 })();
 
 function bodyScrollingToggle() {
-    document.body.classList.toggle("stop-scrolling");
+    document.body.classList.toggle("hidden-scrolling");
 }
 
 
@@ -171,13 +241,13 @@ function bodyScrollingToggle() {
 
 /*-------------------testimonial slider--------------------------*/
 
-(()=>{
+(() => {
     const sliderContainer = document.querySelector(".testi-slider-container"),
-    slides = sliderContainer.querySelectorAll(".testi-item"),
-    slideWidth = sliderContainer.offsetWidth,
-    prevBtn = document.querySelector(".testi-slider-nav .prev"),
-    nextBtn = document.querySelector(".testi-slider-nav .next"),
-    activeSlide = document.querySelector(".testi-item.active");
+        slides = sliderContainer.querySelectorAll(".testi-item"),
+        slideWidth = sliderContainer.offsetWidth,
+        prevBtn = document.querySelector(".testi-slider-nav .prev"),
+        nextBtn = document.querySelector(".testi-slider-nav .next"),
+        activeSlide = document.querySelector(".testi-item.active");
     let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
     console.log(slideIndex);
 
@@ -207,7 +277,7 @@ function bodyScrollingToggle() {
         slider();
     })
 
-    function slider(){
+    function slider() {
         //deactivate existing active slide
         sliderContainer.querySelector(".testi-item.active").classList.remove("active");
         //activate new slide
@@ -215,4 +285,15 @@ function bodyScrollingToggle() {
         sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
     }
     slider();
+})();
+
+
+/*-------------------hide all section except active--------------------------*/
+(() => {
+    const sections = document.querySelectorAll(".section");
+    sections.forEach((section) => {
+        if (!section.classList.contains("active")) {
+            section.classList.add("hide");
+        }
+    })
 })();
